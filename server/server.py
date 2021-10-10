8763a3c9125f0ae21008f3e4da350fde9f8755c2
tstelzle <tarek.stelzle@gmx.de>
2021-10-10 21:21:11 +0200

    Working Database

--- server/server.py
+++ server/server.py
@@ -1,20 +1,30 @@
 import module_controller
 import pymongo
+import time
+import os
 
 from app import api, app
 
 api.add_resource(module_controller.GameController)
 api.add_resource(module_controller.PlayerController)
 
-DOMAIN = "localhost"
-PORT = "27000"
 
 def create_database_connection() -> pymongo.MongoClient:
-    myclient = pymongo.MongoClient(
-        host= [ str(DOMAIN) + ":" + str(PORT)]
-    )
+    USER = os.getenv('MONGO_INITDB_ROOT_USERNAME')
+    PASSWORD = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
 
-    print(myclient.server_info())
+    CONNECTION_URL = "mongodb://{0}:{1}@thegame-db:27017".format(USER, PASSWORD)
+
+    connection_not_established = True
+    while connection_not_established:
+        try:
+            myclient = pymongo.MongoClient(CONNECTION_URL)
+            connection_not_established = False
+        except Exception:
+            print("Timeout Error - Waiting and Retrying")
+            time.sleep(5)
+
+    print("server version:", myclient.server_info()["version"])
 
     return myclient
 
@@ -25,6 +35,7 @@ def create_collection(collection_name: str, mydb):
     return mydb[collection_name]
 
 if __name__ == "__main__":
+    print("main")
     myclient = create_database_connection()
     database = setup_database(myclient)
     collection = create_collection('games', database)
