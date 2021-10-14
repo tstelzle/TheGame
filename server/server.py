adccc6f13f9f2038f5d21e47a7976f97137fea8b
tstelzle <tarek.stelzle@gmx.de>
2021-10-14 23:00:18 +0200

    Added UWSGI

--- server/server.py
+++ server/server.py
@@ -5,5 +5,4 @@ from app import api, app
 api.add_resource(module_controller.GameController)
 api.add_resource(module_controller.PlayerController)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050)
+application = app
